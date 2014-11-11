8985d8c7d333961b524f3145d4b23e33e192e204
Luke Gilson <LukeGilson@Lukes-MacBook-Pro.local>
2014-11-10 23:46:45 -0500

    moved files around, building the html stripper

---
+++ retrieve/connect.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+mysql_connection = mysql.connector.connect(user='root', 
+                              password='root',
+                              host='localhost',
+                              database='uplytics_db')
+
